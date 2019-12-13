a9d7895228fdeeef3240ef004669b1d961c37e13
agamgn <agamgn@163.com>
2019-12-13 09:51:02 +0800

    'header'

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {connect} from 'react-redux'; 
 import { CSSTransition } from 'react-transition-group';
+import  {actionCreators}  from './store';
 import {
     HeaderWrapper,
     Logo,
@@ -12,62 +13,64 @@ import {
     SearchWrapper
 } from './style';
 
-class Header extends Component {
+// 无状态组件
+const Header= (props)=>{
+    return (
+    <HeaderWrapper>
+    <Logo />
+    <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载App</NavItem>
+        <NavItem className='right'>登录</NavItem>
+        <NavItem className='right'>
+            <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
+            <CSSTransition
+            in={props.focused}
+            timeout={200}
+            classNames='slide'
+            >
+                <NavSearch
+                    className={props.focused ? 'focused' : ''}
+                    onFocus={props.handleInputFocus}
+                    onBlur={props.handleInputBlur}
+                >
 
+                </NavSearch>
+            </CSSTransition>
+            <i className={props.focused ? 'focused iconfont' : 'iconfont'}>
+                &#xe614;
+            </i>
+        </SearchWrapper>
+    </Nav>
+    <Addition>
+        <Button className='writting'>
+            <i className="iconfont">&#xe615;</i>
+            写文章</Button>
+        <Button className='reg'>注册</Button>
 
-    render() {
-        return (
-            <HeaderWrapper>
-                <Logo />
-                <Nav>
-                    <NavItem className='left active'>首页</NavItem>
-                    <NavItem className='left'>下载App</NavItem>
-                    <NavItem className='right'>登录</NavItem>
-                    <NavItem className='right'>
-                        <i className="iconfont">&#xe636;</i>
-                    </NavItem>
-                    <SearchWrapper>
-                        <CSSTransition
-                        in={this.props.focused}
-                        timeout={200}
-                        classNames='slide'
-                        >
-                            <NavSearch
-                                className={this.props.focused ? 'focused' : ''}
-                                onFocus={this.props.handleInputFocus}
-                                onBlur={this.props.handleInputBlur}
-                            >
-
-                            </NavSearch>
-                        </CSSTransition>
-                        <i className={this.props.focused ? 'focused iconfont' : 'iconfont'}>
-                            &#xe614;
-						</i>
-                    </SearchWrapper>
-                </Nav>
-                <Addition>
-                    <Button className='writting'>
-                        <i className="iconfont">&#xe615;</i>
-                        写文章</Button>
-                    <Button className='reg'>注册</Button>
-
-                </Addition>
-            </HeaderWrapper>
-        )
-    }
-
-   
+    </Addition>
+</HeaderWrapper>
+)
 }
+
 
 const mapStateToProps=(state)=>{
     return {
-        focused:state.focused
+        focused:state.getIn(['header','focused'])
+      
     }
 }
 
 const mapDispathToProps=(dispatch)=>{
     return {
-
+        handleInputFocus(){
+            dispatch(actionCreators.searchFocus());
+        },
+        handleInputBlur(){
+            dispatch(actionCreators.searchBlur());
+        }
     }
 }
 // export default Header;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,8 @@
-const defaultState={
-    focused:false
-};
+import {combineReducers} from "redux-immutable"
+import {reducer as headerReducer} from "../common/header/store"
 
-export default (state=defaultState,action) =>{
-    return state
-}
+ const reducer= combineReducers({
+    header:headerReducer
+})
+
+export default reducer;
