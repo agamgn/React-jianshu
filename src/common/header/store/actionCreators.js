c9722cf9f6c5ffcd83d0931ae95da0635691c68b
agamgn <agamgn@163.com>
2019-12-13 13:04:48 +0800

    'search'

--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -1,5 +1,8 @@
-
+import {fromJS} from "immutable";
 import * as actionTypes from './constants'
+import axios from 'axios';
+import { constants } from '.';
+
 export const searchFocus=()=>({
     type:actionTypes.SEARCH_FOCUS
 })
@@ -7,3 +10,34 @@ export const searchFocus=()=>({
 export const searchBlur=()=>({
     type:actionTypes.SEARCH_BLUR
 })
+export const mouseEnter=()=>({
+    type:actionTypes.MOUSE_ENTER
+})
+export const mouseLeave=()=>({
+    type:actionTypes.MOUSE_LEAVE
+})
+export const changePageList=(page)=>({
+    type:actionTypes.CHANGE_PAGE,
+    page
+})
+
+
+
+const changeList=(data)=>({
+    type:constants.CHANGE_LIST,
+    data:fromJS(data),
+    totalPage:Math.ceil(data.length/10)
+})
+
+
+
+export const getList =() =>{
+    return (dispatch) =>{
+        axios.get("/api/headerList.json").then((res)=>{
+            const data=res.data;
+            dispatch(changeList(data.data))
+        }).catch(()=>{
+            console.log("error")
+        })
+    }
+}
